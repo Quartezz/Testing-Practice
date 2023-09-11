f29ba8d82d0d3e361e733c7ddd3f4bb58e5c9b86
Quartezz <kamil.machel03@gmail.com>
2023-09-11 22:07:01 +0200

    Edited function to ignore spaces

--- src/reverseString.js
+++ src/reverseString.js
@@ -1,5 +1,5 @@
 function reverseString(string) {
-    return string.split("").reverse().join("");
+    return string.split('').filter(char => char !== ' ').reverse().join('');
 }
 
 module.exports = reverseString;

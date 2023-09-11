1b2db93b5331aeb469b2578640fe352aa5c42a6c
Quartezz <kamil.machel03@gmail.com>
2023-09-11 21:58:12 +0200

    Tested capitalize

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -1,5 +1,17 @@
 const capitalize = require("./capitalize");
 
 test("capitalize first char in a string", () =>{
-    expect(capitalize("banana").toBe("Banana"))
+    expect(capitalize("banana")).toEqual("Banana")
 });
+
+test("do not change an already capitalized string", () => {
+    expect(capitalize("Robot")).toEqual("Robot")
+})
+
+test("work on empty strings", () => {
+    expect(capitalize("")).toEqual("")
+})
+
+test("capitalize a single character string", () => {
+    expect(capitalize("W")).toEqual("W")
+})
